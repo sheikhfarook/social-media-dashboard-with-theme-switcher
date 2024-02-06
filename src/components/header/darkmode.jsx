d1950c1cb8354e17b17755bfc93e2eb640ee6a3d
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-06 13:23:04 +0530

    i complete the little bit of dark mode

--- src/components/header/darkmode.jsx
+++ src/components/header/darkmode.jsx
@@ -1,5 +1,16 @@
 const DarkMode = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between ">
+      <div className="leading-2">
+        <div className="text-[2rem]">Social Media Dashboard</div>
+        <div className="text-[1rem]">Total Followers:23,004</div>
+      </div>
+      <div className="flex">
+        <div>Dark Mode</div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default DarkMode;
