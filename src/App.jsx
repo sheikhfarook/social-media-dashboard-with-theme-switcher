937c0f1f3801dba9730cd125032a596a73545543
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-06 13:22:35 +0530

    i create the another components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import DarkMode from "./components/header/darkmode";
+import DashBoard from "./components/header/dashboard";
 
 function App() {
   return (
     <div>
       <DarkMode />
+      <DashBoard />
     </div>
   );
 }
