4b77dc623a1b19acad6d0fbf19dbd3d808058cdf
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-08 15:20:41 +0530

    complete desktop style

--- src/components/header/darkmode.jsx
+++ src/components/header/darkmode.jsx
@@ -1,12 +1,16 @@
 const DarkMode = () => {
   return (
-    <div className="flex justify-between ">
+    <div className="flex justify-between text-[#FFF] ">
       <div className="leading-2">
-        <div className="text-[2rem]">Social Media Dashboard</div>
-        <div className="text-[1rem]">Total Followers:23,004</div>
+        <div className="text-[2rem] text-[#FFF] font-[700]">
+          Social Media Dashboard
+        </div>
+        <div className="text-[1rem] text-[#8c98c6] font-[700] leading-[16.94px] ">
+          Total Followers:23,004
+        </div>
       </div>
-      <div className="flex">
-        <div>Dark Mode</div>
+      <div className="flex my-auto">
+        <div className="text-[14px] font-[700] text-[#8c98c6]">Dark Mode</div>
         <div></div>
       </div>
     </div>
