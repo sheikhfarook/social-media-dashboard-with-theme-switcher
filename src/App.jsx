15c1627ac5a1939c389c0ffd2e5ad7375d7a8d14
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-08 15:17:41 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import "./App.css";
 import DarkMode from "./components/header/darkmode";
-import DashBoard from "./components/header/dashboard";
 
 function App() {
   return (
-    <div>
       <DarkMode />
-      <DashBoard />
     </div>
   );
 }
