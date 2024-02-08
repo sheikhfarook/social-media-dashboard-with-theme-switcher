f7a53b841007cb71f52465de18ead927eb1cf719
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-08 15:15:33 +0530

    images added

---
+++ src/components/data/overview.jsx
@@ -0,0 +1,82 @@
+import Facebook from "../../assets/path.svg";
+import Instagram from "../../assets/shape.svg";
+import twitter from "../../assets/paths.svg";
+import youtube from "../../assets/shapes.svg";
+import arrow from "../../assets/arrow.svg";
+import downarrow from "../../assets/down-arrow.svg";
+
+export const overView = [
+  {
+    id: 1,
+    text: "Page Views",
+    colors: "bluecolor",
+    icon1: Facebook,
+    rank: 87,
+    icon2: arrow,
+    percentage: "3%",
+  },
+  {
+    id: 2,
+    text: "Likes",
+    colors: "redcolor",
+    icon1: Facebook,
+    rank: 52,
+    icon2: downarrow,
+    percentage: "2%",
+  },
+  {
+    id: 3,
+    text: "Likes",
+    colors: "bluecolor",
+    icon1: Instagram,
+    rank: 5462,
+    icon2: arrow,
+    percentage: "2257%",
+  },
+  {
+    id: 3,
+    text: "Profile views",
+    colors: "bluecolor",
+    icon1: Instagram,
+    rank: "52k",
+    icon2: arrow,
+    percentage: "1357%",
+  },
+  {
+    id: 5,
+    text: "Retweets",
+    colors: "bluecolor",
+    icon1: twitter,
+    rank: 117,
+    icon2: arrow,
+    percentage: "303%",
+  },
+  {
+    id: 6,
+    text: "Likes",
+    colors: "bluecolor",
+    icon1: twitter,
+    rank: 507,
+    icon2: arrow,
+    percentage: "553%",
+  },
+
+  {
+    id: 7,
+    text: "Likes",
+    icon1: youtube,
+    colors: "redcolor",
+    rank: 107,
+    icon2: downarrow,
+    percentage: "19%",
+  },
+  {
+    id: 8,
+    text: "Total Views",
+    icon1: youtube,
+    colors: "redcolor",
+    rank: 1407,
+    icon2: downarrow,
+    percentage: "12%",
+  },
+];
