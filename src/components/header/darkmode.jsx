5bcee8e29eccc1878bfd04d8369ffaed70f68c73
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-08 15:32:00 +0530

    complete mobile design

--- src/components/header/darkmode.jsx
+++ src/components/header/darkmode.jsx
@@ -1,13 +1,20 @@
 const DarkMode = () => {
   return (
-    <div className="flex justify-between text-[#FFF] ">
-      <div className="leading-2">
-        <div className="text-[2rem] text-[#FFF] font-[700]">
+    <div
+      className="flex justify-between text-[#FFF] max-sm:flex-col max-sm:space-y-4
+     max-sm:w-[326px] max-sm:m-auto">
+      <div className="leading-">
+        <div className="text-[2rem] text-[#FFF] font-[700] max-sm:text-[24px]">
           Social Media Dashboard
         </div>
-        <div className="text-[1rem] text-[#8c98c6] font-[700] leading-[16.94px] ">
+        <div
+          className="text-[1rem] text-[#8c98c6] font-[700] leading-[16.94px] 
+        max-sm:text-[14px] ">
           Total Followers:23,004
         </div>
+      </div>
+      <div className=" opacity-35">
+        <hr />
       </div>
       <div className="flex my-auto">
         <div className="text-[14px] font-[700] text-[#8c98c6]">Dark Mode</div>
