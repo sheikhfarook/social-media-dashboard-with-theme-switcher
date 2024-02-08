e05c773e264bce6dd0278b991d12f63cf5f9deb6
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-08 15:36:08 +0530

    create the json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import DarkMode from "./components/header/darkmode";
+import DashBoardComponent from "./components/header/dashboard/dashboard";
 
 function App() {
   return (
+    <div className="md:w-[1110px] m-auto my-[5%] font-inter max-sm:w-[326px] ">
       <DarkMode />
+      <DashBoardComponent />
     </div>
   );
 }
---
+++ src/components/data/data.jsx
@@ -0,0 +1,49 @@
+import FacebookIcon from "../../assets/path.svg";
+import twitterIcon from "../../assets/paths.svg";
+import instagramIcon from "../../assets/shape.svg";
+import youtubeIcon from "../../assets/shapes.svg";
+import arrow from "../../assets/arrow.svg";
+import downarrow from "../../assets/down-arrow.svg";
+
+export const dashboardData = [
+  {
+    id: 1,
+    profile: FacebookIcon,
+    name: "facebook",
+    Username: "@nathanf",
+    number: "1987",
+    popular: "FOLLOWERS",
+    profile2: arrow,
+    date: "12 Today",
+  },
+  {
+    id: 2,
+    profile: twitterIcon,
+    name: "twitter",
+    Username: "@nathanf",
+    number: "1044",
+    popular: "FOLLOWERS",
+    profile2: arrow,
+    date: "99 Today",
+  },
+  {
+    id: 3,
+    profile: instagramIcon,
+    name: "instagram",
+    Username: "@realnathanf",
+    number: "11K",
+    popular: "FOLLOWERS",
+    profile2: arrow,
+    date: "1099 Today",
+  },
+  {
+    id: 4,
+    profile: youtubeIcon,
+    name: "youtube",
+    Username: "Nathan F.",
+    number: "8239",
+    popular: "SUBSCRIBERS",
+    profile2: downarrow,
+    date: "144 Today",
+  },
+];
