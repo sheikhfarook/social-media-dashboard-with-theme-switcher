30e9250bb0396f0d05b8504558539776a3370482
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-08 15:42:46 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import DarkMode from "./components/header/darkmode";
 import DashBoardComponent from "./components/header/dashboard/dashboard";
+import TodaysView from "./components/todaysview";
 
 function App() {
   return (
     <div className="md:w-[1110px] m-auto my-[5%] font-inter max-sm:w-[326px] ">
       <DarkMode />
       <DashBoardComponent />
+      <TodaysView />
     </div>
   );
 }
